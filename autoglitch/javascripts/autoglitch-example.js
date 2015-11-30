ec412321c2491d47d700340819f890192a6edafd
Andy <andy.beering@tobi.com>
2015-11-30 11:28:17 -0800

    cleaned up and added examle tag to autogtlich example

--- autoglitch/javascripts/autoglitch-example.js
+++ autoglitch/javascripts/autoglitch-example.js
@@ -31,14 +31,28 @@ function replaceImage(){
 }
 
 function setImageOptions(image_element){
+  var tag_options_string = '';
   $('.option-value').each(function(idx,el){
     if(el.value){
+      if($(el).hasClass('validate')){
+        if(parseInt(el.value) < 0 || parseInt(el.value) > 99){
+          $(el).val('');
+          return false;
+        }
+      }
       option_name = $(el).data('option-name');
       option_name = 'data-' + option_name;
+      tag_options_string = tag_options_string + ' ' + option_name + '="' + el.value + '"';
       image_element.attr(option_name,el.value);
     }
   });
+  updateExampleTag(tag_options_string);
   restartAutoglitch();
+}
+
+function updateExampleTag(tag_options_string){
+  example_tag = '<img src="foo.jpg" class="autoglitch"' + tag_options_string + ' />';
+  $('.image-tag').text(example_tag);
 }
 
 function restartAutoglitch(){
